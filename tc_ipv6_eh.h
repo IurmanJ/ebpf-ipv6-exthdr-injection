0cf2066024b80ad73f65189d562069c4a9211b87
Justin Iurman <justin.iurman@uliege.be>
2023-09-02 19:36:15 +0200

    WIP user program (cont')
    WIP user program (cont')

--- tc_ipv6_eh.h
+++ tc_ipv6_eh.h
@@ -2,15 +2,15 @@
 #define TC_IPV6_EH_H
 
 #define MAP_NAME	eh6_map
-#define EH_MAX_BYTES	((255 + 1) << 3)
+#define EH_MAX_BYTES	((255 + 1) << 3) /* DO NOT MODIFY */
 
 struct exthdr_t {
 	struct bpf_spin_lock lock;
 	__u8 type_first_eh;
 	__u32 off_last_nxthdr;
 	__u32 bytes_len;
-#define MIN_BYTES 8
-#define MAX_BYTES EH_MAX_BYTES /* Feel free to increase */
+#define MIN_BYTES	8 /* DO NOT MODIFY */
+#define MAX_BYTES	EH_MAX_BYTES /* Feel free to modify */
 	__u8 bytes[MAX_BYTES];
 };
 
--- tc_ipv6_eh_user.c
+++ tc_ipv6_eh_user.c
@@ -6,18 +6,12 @@
 #include <linux/rpl.h>
 #include <linux/seg6.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "tc_ipv6_eh.h"
 
 #define MAP_DIR			"/sys/fs/bpf/tc/globals/"
-
-#define NEXTHDR_HOP		0	/* Hop-by-hop options header */
-#define NEXTHDR_ROUTING		43	/* Routing header */
-#define NEXTHDR_FRAGMENT	44	/* Fragmentation header */
-#define NEXTHDR_ESP		50	/* Encapsulating security payload */
-#define NEXTHDR_AUTH		51	/* Authentication header */
-#define NEXTHDR_DEST		60	/* Destination options header */
 
 #define ARG_ENABLE		"--enable"
 #define ARG_DISABLE		"--disable"
@@ -38,6 +32,16 @@
 #define MIN_BYTES_RH4		(sizeof(struct ipv6_sr_hdr) + sizeof(struct in6_addr))
 #define MIN_BYTES_AH		(sizeof(struct ip_auth_hdr) + 4)
 #define MIN_BYTES_ESP		(sizeof(struct ip_esp_hdr) + 8)
+#define MAX_BYTES_AH		((255 + 2) << 2)
+#define MAX_BYTES_ESP		((MAX_BYTES) & (-8))
+
+#define FLAGS_HBH		(1 << 0)
+#define FLAGS_DEST1		(1 << 1)
+#define FLAGS_RH		(1 << 2)
+#define FLAGS_FRAG		(1 << 3)
+#define FLAGS_AH		(1 << 4)
+#define FLAGS_ESP		(1 << 5)
+#define FLAGS_DEST2		(1 << 6)
 
 #define STR(x)			STR2(x)
 #define STR2(x)			#x
@@ -56,8 +60,8 @@ void print_help(char *prog_name)
 	printf(" | %s %ld..%d", ARG_RH3, MIN_BYTES_RH3, EH_MAX_BYTES);
 	printf(" | %s %ld..%d", ARG_RH4, MIN_BYTES_RH4, EH_MAX_BYTES);
 	printf(" | %s | %s", ARG_FRAG_A, ARG_FRAG_NA);
-	printf(" | %s %ld..%d", ARG_AH, MIN_BYTES_AH, 123/*TODO*/);
-	printf(" | %s %ld..%d", ARG_ESP, MIN_BYTES_ESP, MAX_BYTES);
+	printf(" | %s %ld..%d", ARG_AH, MIN_BYTES_AH, MAX_BYTES_AH);
+	printf(" | %s %ld..%d", ARG_ESP, MIN_BYTES_ESP, MAX_BYTES_ESP);
 	printf(" }\n\n");
 	printf("If a size is required, it MUST be an 8-octet multiple.\n\n");
 	printf("Accepted chaining order, as per RFC8200 sec4.1:\n");
@@ -70,57 +74,128 @@ void print_help(char *prog_name)
 	printf(" - Destination Options header\n");
 }
 
+void print_missing_size(char *arg)
+{
+	printf("\nMissing size for \"%s\"\n", arg);
+}
+
+void print_invalid_size(char *arg)
+{
+	printf("\nInvalid size for \"%s\"\n", arg);
+}
+
+void print_not_rfc8200_compliant()
+{
+	printf("\nNot RFC8200 compliant, use \"--force\" to override\n");
+}
+
+void print_size_overflow(unsigned int max)
+{
+	printf("\nCannot exceed the total maximum size of %u bytes\n", max);
+}
+
+unsigned char valid_size(char *s, unsigned int min, unsigned int max, long *val)
+{
+	char *p;
+
+	*val = strtol(s, &p, 10);
+	if (!*val || strlen(p) > 0 || *val < min || *val > max || *val%8 != 0)
+		return 0;
+
+	return 1;
+}
+
 int main(int argc, char **argv)
 {
-	unsigned char rfc_order = 1, flags = 0, forced = 0;
-	unsigned int i, len = 0, key = 0;
+	unsigned char flags = 0, force = 0;
 	struct exthdr_t exthdr = {};
+	unsigned int i, key = 0;
 	int fd, ret;
+	long n;
 
 	if (argc < 2 || (argc == 2 && strcmp(argv[1], ARG_DISABLE)) ||
 	    (argc > 2 && strcmp(argv[1], ARG_ENABLE)) ||
-	    (argc == 3 && !strcmp(argv[2], ARG_FORCE))) {
-		print_help(argv[0]);
-		return 1;
-	}
+	    (argc == 3 && !strcmp(argv[2], ARG_FORCE)))
+		goto error_out;
 
 	if (!strcmp(argv[1], ARG_DISABLE))
 		goto update_map;
 
 	i = 2;
 	if (!strcmp(argv[2], ARG_FORCE)) {
-		i++;
-		forced = 1;
+		i += 1;
+		force = 1;
 	}
+
+	exthdr.off_last_nxthdr = MAX_BYTES;//TODO
 
 	for(i; i < argc; i++) {
-		printf("argv[%d] = %s\n", i, argv[i]);
-
 		if (!strcmp(argv[i], ARG_HBH)) {
-			/*if (flags)
-				rfc_order = 0;*/
+			if (i+1 == argc) {
+				print_missing_size(ARG_HBH);
+				goto error_out;
+			}
+
+			i += 1;
+			if (!valid_size(argv[i], MIN_BYTES, EH_MAX_BYTES, &n)) {
+				print_invalid_size(ARG_HBH);
+				goto error_out;
+			}
+
+			if (!force && flags) {
+				print_not_rfc8200_compliant();
+				goto error_out;
+			}
+
+			if (exthdr.bytes_len + n > MAX_BYTES) {
+				print_size_overflow(MAX_BYTES);
+				goto error_out;
+			}
+
+			struct ipv6_opt_hdr *opt = (void *)&exthdr.bytes[exthdr.bytes_len];
+			opt->hdrlen = (n >> 3) - 1;
+			//TODO insert in exthdr.bytes
+			/*
+			for(__u16 i = 2; i + 1 < *len; i += 257)
+			{
+				exthdr->bytes[i] = 0x1e;
+				exthdr->bytes[i + 1] = MIN(*len - i - 2, 255);
+			}
+			*/
+
+			//TODO if there is a previous EH with a next hdr field, set it to IPPROTO_HOPOPTS, else set exthdr.type_first_eh to IPPROTO_HOPOPTS
+			//TODO set exthdr.off_last_nxthdr to the hbh nexthdr field offset
+			/*
+			exthdr.type_first_eh = NEXTHDR_HOP;
+			exthdr.off_last_nxthdr = 0; //offsetof(struct ipv6_opt_hdr, nexthdr);
+			*/
+
+			exthdr.bytes_len += n;
+			flags |= FLAGS_HBH;
 		} else if (!strcmp(argv[i], ARG_DEST)) {
+			//IPPROTO_DSTOPTS
 		} else if (!strcmp(argv[i], ARG_RH0)) {
+			//IPPROTO_ROUTING
 		} else if (!strcmp(argv[i], ARG_RH2)) {
+			//IPPROTO_ROUTING
 		} else if (!strcmp(argv[i], ARG_RH3)) {
+			//IPPROTO_ROUTING
 		} else if (!strcmp(argv[i], ARG_RH4)) {
+			//IPPROTO_ROUTING
 		} else if (!strcmp(argv[i], ARG_FRAG_A)) {
+			//IPPROTO_FRAGMENT
 		} else if (!strcmp(argv[i], ARG_FRAG_NA)) {
+			//IPPROTO_FRAGMENT
 		} else if (!strcmp(argv[i], ARG_AH)) {
+			//IPPROTO_AH
+			//ah size = (total bytes / 4) - 2
 		} else if (!strcmp(argv[i], ARG_ESP)) {
+			//IPPROTO_ESP
 		} else {
-			printf("Unexpected argument \"%s\"\n", argv[i]);
-			print_help(argv[0]);
-			return 1;
+			printf("\nUnexpected argument \"%s\"\n", argv[i]);
+			goto error_out;
 		}
 	}
-	/*
-	exthdr.type_first_eh = NEXTHDR_HOP;
-	exthdr.off_last_nxthdr = 0; //offsetof(struct ipv6_opt_hdr, nexthdr);
-	exthdr.bytes_len = 8;
-	exthdr.bytes[2] = 0x01;
-	exthdr.bytes[3] = 0x04;
-	*/
 
 update_map:
 	/* Retrieve the array map */
@@ -140,4 +215,8 @@ update_map:
 
 	close(fd);
 	return 0;
+
+error_out:
+	print_help(argv[0]);
+	return 1;
 }
